44da95dcc7611eed659f7c3b35831b06ddb5658a
Dudko Kate <ekaterina_dr@tut.by>
2022-02-22 21:37:36 +0300

    added new tasks

---
+++ Global Scope and Functions.js
@@ -0,0 +1,17 @@
+function fun1() {
+    return 5;
+}
+var oopsGlobal = fun1();
+let myGlobal = 10;
+// Only change code above this line
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
---
+++ Shopping List.js
@@ -0,0 +1,2 @@
+const myList = [];
+myList.push(["Milk", 5], ["Tea", 10], ["Coca-Cola", 5],["Meat", 20], ["Cheese", 10]);
