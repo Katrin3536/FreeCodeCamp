dbcd77a253adf8f546284be1cb57f4298195ea3c
Dudko Kate <ekaterina_dr@tut.by>
2022-03-20 21:57:59 +0300

    added new task

---
+++ Adding a Default Option in Switch Statements.js
@@ -0,0 +1,19 @@
+function switchOfStuff(val) {
+    let answer = "";
+    switch (val) {
+        case "a":
+            return "apple";
+            break;
+        case "b":
+            return "bird";
+            break;
+        case "c":
+            return "cat";
+            break;
+        default:
+            return "stuff";
+            break
+    };
+
+    return answer;
+}
